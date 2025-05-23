cf4b8069addc603dcd16116e3bbc9e4e08f8cf8e
gopi bharadwaj <gopipathapalli@gmail.com>
2025-05-23 16:14:06 +0530

    code added

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,10 @@
 
 echo "all variables  passed  to the scripy: $@ "
 echo "Number of varibles: $#"
+echo "script name: $0"
+echo "current directory: $pwd"
+echo "user running of scrippt: $user"
+echo "Home direcetory of user: $HOME"
+echo "pid of the script: $$"
+sleep 10 &
+echo "pid of last command in background: $!"
