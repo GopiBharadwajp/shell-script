81dfc61b8abe98346a429a5f0fa779e4d22aec35
gopi bharadwaj <gopipathapalli@gmail.com>
2025-05-23 13:15:47 +0530

    code added

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: hey Musk , how are you"
+echo "Musk:: Hello Trump, i am fine . how are you doing "
+echo "Trump: i am fine too. what's up ?"
+echo "Musk :: Nothing , just going to Mars now, will you come?"
+echo "Trump:: sorry , you carry on! I will come once uou come back"
