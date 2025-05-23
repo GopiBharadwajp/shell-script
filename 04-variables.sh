5e8753962ebd8dd99d9f7826d10478953d896338
gopi bharadwaj <gopipathapalli@gmail.com>
2025-05-23 13:32:52 +0530

    code added

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PERSON1=
+PERSON2=
+
+
+
+echo "$PERSON1: hey $PERSON2 , how are you"
+echo "$PERSON2:: Hello $PERSON1, i am fine . how are you doing "
+echo "$PERSON1: i am fine too. what's up ?"
+echo "$PERSON2 :: Nothing , just going to Mars now, will you come?"
+echo "$PERSON1:: sorry , you carry on! I will come once uou come back"
