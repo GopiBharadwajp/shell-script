f7ffb1fbc6a472941490022965f3dbca6e224b4f
gopi bharadwaj <gopipathapalli@gmail.com>
2025-05-24 06:43:56 +0530

    code added

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -6,10 +6,19 @@ if [ $USERID -ne 0 ]
 then
 echo "ERROR:: Plese run this script with root acess"
  exit 1
- 
+
 else
 echo "you are running with root acess"
 
 fi
 
 dnf install mysql -y
+if [ $? =0 ]
+
+then
+ echo "installing Mysql is .... SUCESS"
+
+ else
+ echo "installing my sql is ... failuer"
+  #exit 1
+ fi
