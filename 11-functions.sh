71ca152c69801d13e34db31bae39893adb08625c
gopi bharadwaj <gopipathapalli@gmail.com>
2025-05-24 18:15:23 +0530

    code added

--- 11-functions.sh
+++ 11-functions.sh
@@ -6,29 +6,47 @@ if [ id -ne 0 ]
 
 then 
 echo "you need to run in root user "
-
 else 
-
 echo "run with root access"
-
 fi
-
-dnf install mysql
+VALIDATE(){
+    if[$? -ne 0 ]
+ then 
+ echo "installing $2 is ....sucess"
+else 
+echo  "installing $2 is failuer"
 exit 1
 fi
+}
 
-if [ #? -ne 0]
-
+dnf list installed mysql
+if[ $? -ne 0 ]
 then 
-echo "installing my sql sucesss"
-
+echo "my sql not instelled  going to intall it"
+dnf install mysql -y
+VALIDATE $? "mysql"
+fi
 else 
-echo  "my sql not installed   g=failuer"
-
-exit 1
+echo "my sql alredy installed nothimg to do"
 fi
 
+dnf list installed python3
+if[ $? -ne 0 ]
+then 
+echo "my sql not instelled  going to intall it"
+dnf install python3 -y
+VALIDATE $? "python3"
 else 
-echo "my sql alredy installed"
+echo "python3 alredy installed nothimg to do"
+fi
+
+dnf list installed nginx
+if[ $? -ne 0 ]
+then 
+echo "my sql not instelled  going to intall it"
+dnf install nginx -y
+VALIDATE $? ""nginx
+else 
+echo "nginx alredy installed nothimg to do"
 fi
 
